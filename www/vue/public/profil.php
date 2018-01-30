2b47ce61b21ab054cd3065fe5d9b44550ad65873
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-01-30 16:07:03 -0500

    Ajout vue profil et modification style_general.css. References #13

---
+++ www/vue/public/profil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8"/>
+	<link rel = "stylesheet" href = "style/style_general.css">		
+	<title> Profil d'un utilisateur</title>
+</head>
+<body>
+	<header>
+		<h1>Profil de Toto</h1>
+    </header>	
+    <div id="menu">
+    <ul id="onglets">
+        <li class="active"><a href="Accueil.html"> Informations </a></li>
+        <li><a href="Forums.html"> Ventes </a></li>
+        <li><a href="Livre_or.html"> Achats </a></li>
+        <li><a href="Equipe.html"> Avis reçus </a></li>
+        <li><a href="Inscription.html"> Avis donnés </a></li>
+        <li><a href="Connexion.html"> Modifier </a></li>
+    </ul>
+</div>
+
+</body>
+</html>
