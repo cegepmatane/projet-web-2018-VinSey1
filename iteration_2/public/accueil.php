babe52a7d29a6915cb809a1085429d4a30d9337b
Fromage <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-01-30 11:10:23 -0500

    màj references #11

--- iteration_2/public/accueil.php
+++ iteration_2/public/accueil.php
@@ -1,7 +1,8 @@
 <!doctype html>
 <html lang="fr">
 <head>
-	<meta charset="utf-8">
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link rel = "stylesheet" href = "decoration/style_general.css">		
 	<title> Page dev </title>
 </head>
 <body>
@@ -9,6 +10,10 @@
 		<h1>Page de développement</h1>
 	</header>
 		
+	<h1>
+		Projet de site de e-commerce par des étudiants du Cégep de Matane
+	</h1>
+	<img src=./illustrations/moyen/cegepmatane_logo.png>
 	
 </body>
 </html>
