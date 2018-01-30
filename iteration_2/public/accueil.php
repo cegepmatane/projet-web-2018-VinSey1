dc8dc3dd215c22b68586ffaed9051f055112d250
Fromage <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-01-30 10:13:26 -0500

    début #11

---
+++ iteration_2/public/accueil.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title> Page dev </title>
+</head>
+<body>
+	<header>
+		<h1>Page de développement</h1>
+	</header>
+		
+	
+</body>
+</html>
