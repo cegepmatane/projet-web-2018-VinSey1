44a29281f928c91bf9b4dc86b4859366cd137843
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-02-01 21:16:52 -0500

    ossature de la vue catalogue references #15

---
+++ www/vue/public/catalogue.php
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link rel = "stylesheet" href = "decoration/style_general.css">		
+	<title> Page dev catalogue </title>
+</head>
+<body>
+	<header>
+		<i>Page de développement Catalogue</i>
+		<p id="titre_survie_etudiante"> Survie étudiante</p>
+		<div class="logo_Cegep_Matane">
+			<img src=./illustrations/petit/cegepmatane_logo_petit.png>
+		</div>
+	</header>
+
+	
+	
+	
+</body>
+</html>
