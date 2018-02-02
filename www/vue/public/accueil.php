32483b5c7ca6858a1d5bcbcc233a9b285266a782
Fromage <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-02-02 09:23:11 -0500

    avencee references #11

--- www/vue/public/accueil.php
+++ www/vue/public/accueil.php
@@ -1,17 +1,41 @@
 <? php
 	// Données test
 	$produit1 = new stdClass();
-	$produit1->identifiantDeVente = 1;
+	$produit1->identifiantDeVente = "ez1ezac15hy8j";
 	$produit1->identifiantVendeur = 04519875102;
 	$produit1->titreDeVente = "Casserole en metal";
-	$produit1->categorie = 0;  // 0 : casserole
+	$produit1->categorie = 0;  // 0 : Ustensiles de cuisine
 	$produit1->prix = 20.0;
-	$produit->identifiantMonnaie = 0;  // 0 : dollars
-	$produit->descriptionProduit = "Bonjour, je vends cette casserole qui ne me sert plus, j'habite
+	$produit1->identifiantMonnaie = 0;  // 0 : dollars
+	$produit1->descriptionProduit = "Bonjour, je vends cette casserole qui ne me sert plus, j'habite
 	actuellement au Cégep de Matane";
-	$produit->detailsVente = "Je ne livre pas et je n'envoie pas par la poste, il faudra venir la chercher à Matane au Cégep";
-	$produit->adresse = "616 Avenue du Saint-Rédempteur G4W 1L1 Matane QC Québec";
+	$produit1->detailsVente = "Je ne livre pas et je n'envoie pas par la poste, il faudra venir la chercher à Matane au Cégep";
+	$produit1->adresse = "616 Avenue du Saint-Rédempteur G4W 1L1 Matane QC Québec";
 
+	$produit2 = new stdClass();
+	$produit2->identifiantDeVente = "wr54u8ix1v18r9";
+	$produit2->identifiantVendeur = 04524803548;
+	$produit2->titreDeVente = "Ensemble de couverts";
+	$produit2->categorie = 0;  // 0 : ustensiles de cuisine
+	$produit2->prix = 12.0;
+	$produit2->identifiantMonnaie = 0;  // 0 : dollarss
+	$produit2->descriptionProduit = "Bonjour, je vends cet ensemble de couverts, ils sont en parfait état";
+	$produit2->detailsVente = "Je peux vous les apporter si vous habitez à québec, sinon il va falloira aller les chercher vous-même";
+	$produit2->adresse = "616 Avenue de la Poutine 421 ";
+	
+	$produit3 = new stdClass();
+	$produit3->identifiantDeVente = "d2j8e62g1j7y9s";
+	$produit3->identifiantVendeur = 045215249;
+	$produit3->titreDeVente = "Casserole";
+	$produit3->categorie = 0;  // 0 : ustensiles de cuisine
+	$produit3->prix = 12.0;
+	$produit3->identifiantMonnaie = 0;  // 0 : dollarss
+	$produit3->descriptionProduit = "Bonjour, je vends cet ensemble de couverts, ils sont en parfait état";
+	$produit3->detailsVente = "Je peux vous les apporter si vous habitez à québec, sinon il va falloira aller les chercher vous-même";
+	$produit3->adresse = "616 Avenue de la Poutine 421 ";
+	
+	
+	
 ?>
 
 
