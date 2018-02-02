74de5771e5effd1125a048dc1f7224b24aa4f008
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-02-02 10:53:46 -0500

    base de la page de vente references #16

---
+++ www/vue/public/vente.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Vente</title>
+	<link rel="stylesheet" type="text/css" href="decoration/style_general.css">	
+</head>
+<body>
+	<header>
+		<h1 id="titre_general">Vente d'objet</h1>
+		<nav id="navigation-menu">
+		</nav>
+	</header>
+	
+
+	
