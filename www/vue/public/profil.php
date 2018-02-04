ad2ff3aef482625503c685f3a457ccbb310fb0c2
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-02-04 16:20:41 -0500

    Mise à jour css Vue Profil References #13

--- www/vue/public/profil.php
+++ www/vue/public/profil.php
@@ -1,21 +1,21 @@
 <?php 
 	// Création des données test
-	$profil1 = new stdClass();
-	$profil1->id = 1;
-	$profil1->nom = "Baltz";
-    $profil1->prenom = "Eliott";
-    $profil1->pseudonyme = "Ace57880";
-    $profil1->email = "eliott.baltz@gmail.com";
-    $profil1->adresse = "616 Avenue Saint Rédempteur";
-    $profil1->codepostal = "G4W 1L1 QC";
-    $profil1->pays = "Canada";
-    $profil1->nbachats = "6";
-    $profil1->nbventes = "54";
-    $profil1->karma = "100%";
-    $profil1->illustration = "profil.png";
-    $profil1->age = "20";
-    $profil1->ville = "Matane";
-    $profil1->telephone = "418-562-2609";
+	$profil = new stdClass();
+	$profil->id = 1;
+	$profil->nom = "Baltz";
+    $profil->prenom = "Eliott";
+    $profil->pseudonyme = "Ace57880";
+    $profil->email = "eliott.baltz@gmail.com";
+    $profil->adresse = "616 Avenue Saint Rédempteur";
+    $profil->codepostal = "G4W 1L1 QC";
+    $profil->pays = "Canada";
+    $profil->nbachats = "6";
+    $profil->nbventes = "54";
+    $profil->karma = "100%";
+    $profil->illustration = "profil.png";
+    $profil->age = "20";
+    $profil->ville = "Matane";
+    $profil->telephone = "418-562-2609";
 
 ?>
 <!DOCTYPE html>
@@ -25,76 +25,74 @@
 	<link rel = "stylesheet" href = "decoration/style_general.css">		
 	<title> Profil d'un utilisateur</title>
 </head>
-<body id="generalprofil">
+<body id="general-profil">
 <header>
-	<h1 id="titreprofil">Profil de <?=$profil1->pseudonyme?></h1>
+	<h1 id="titre-profil">Profil de <?=$profil->pseudonyme?></h1>
 </header>	
-<div id="menu">
-    <ul id="onglets">
-        <li class="active"><a href="Accueil.html"> Informations </a></li>
-        <li><a href="Accueil.html"> Ventes  </a></li>
-        <li><a href="Livre_or.html"> Achats </a></li>
-        <li><a href="Equipe.html"> Avis reçus </a></li>
-        <li><a href="Inscription.html"> Avis donnés </a></li>
-        <li><a href="Connexion.html"> Modifier </a></li>
-    </ul>
-</div>
-<div id="contenu">
-    <img id="imgprofil" src="illustrations/tests/<?=$profil1->illustration?>"/>
-    <table id="informations">
+<ul id="onglets-profil">
+    <li class="active"><a href="Accueil.html"> Informations </a></li>
+    <li><a href="Accueil.html"> Ventes  </a></li>
+    <li><a href="Livre_or.html"> Achats </a></li>
+    <li><a href="Equipe.html"> Avis reçus </a></li>
+    <li><a href="Inscription.html"> Avis donnés </a></li>
+    <li><a href="Connexion.html"> Modifier </a></li>
+</ul>
+<div id="contenu-profil">
+    <img id="image-profil" src="illustrations/tests/<?=$profil->illustration?>"/>
+    <table id="informations-profil">
         <tr>
-            <td class="tdprofil">
-                Pseudonyme : <?=$profil1->pseudonyme?>
+            <td class="td-profil">
+                Pseudonyme : <?=$profil->pseudonyme?>
             </td>
         </tr>
         <tr>
-            <td class="tdprofil">
-                Nom : <?=$profil1->nom?>
+            <td class="td-profil">
+                Nom : <?=$profil->nom?>
             </td>
-            <td class="tdprofil">
-                Prénom : <?=$profil1->prenom?>
-            </td>
-        </tr>
-        <tr>
-            <td class="tdprofil">
-                Âge : <?=$profil1->age?>
-        <tr>
-            <td class="tdprofil">
-                Adresse : <?=$profil1->adresse?>
+            <td class="td-profil">
+                Prénom : <?=$profil->prenom?>
             </td>
         </tr>
         <tr>
-            <td class="tdprofil">
-                Ville : <?=$profil1->ville?>
-            </td>
-            <td class="tdprofil">
-                Code Postal : <?=$profil1->codepostal?>
+            <td class="td-profil">
+                Âge : <?=$profil->age?>
+        <tr>
+            <td class="td-profil">
+                Adresse : <?=$profil->adresse?>
             </td>
         </tr>
         <tr>
-            <td class="tdprofil">
-                Pays : <?=$profil1->pays?>
+            <td class="td-profil">
+                Ville : <?=$profil->ville?>
+            </td>
+            <td class="td-profil">
+                Code Postal : <?=$profil->codepostal?>
             </td>
         </tr>
         <tr>
-        <td class="tdprofil">
-                E-mail : <?=$profil1->email?>
+            <td class="td-profil">
+                Pays : <?=$profil->pays?>
             </td>
         </tr>
         <tr>
-        <td class="tdprofil">
-                Téléphone : <?=$profil1->telephone?>
+        <td class="td-profil">
+                E-mail : <?=$profil->email?>
+            </td>
+        </tr>
+        <tr>
+        <td class="td-profil">
+                Téléphone : <?=$profil->telephone?>
             </td>
         </tr>
     </table>
-    <div class="ventesachatskarma">
+    <div class="statistiques-profil">
         <table>
             <tr>
                 <td>
                     Ventes :
                 </td>
                 <td>
-                    <?=$profil1->nbventes?>
+                    <?=$profil->nbventes?>
                 </td>
             </tr>
             <tr>
@@ -102,7 +100,7 @@
                     Achats :
                 </td>
                 <td>   
-                    <?=$profil1->nbachats?>
+                    <?=$profil->nbachats?>
                 </td>
             </tr>
             <tr>
@@ -110,12 +108,12 @@
                     Karma :
                 </td>
                 <td>
-                    <?=$profil1->karma?>
+                    <?=$profil->karma?>
                 </td>
             </tr>
     </table>
     </div>
-    <input class = "boutonprofil" type="button" value="Modifier vos informations">
+    <input class = "bouton-profil" type="button" value="Modifier vos informations">
 </div>
 </body>
 </html>
