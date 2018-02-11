e378cd115ab51f92865ad58f7f1fee18d2661729
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-02-11 16:51:24 -0500

    changement d'hote et d'utilisateur references #25 closes #25

--- www/configuration/configuration.php
+++ www/configuration/configuration.php
@@ -7,10 +7,10 @@ define( "OBJET_MODELE", $_SERVER["DOCUMENT_ROOT"]."/modele/Objet.php"   );
 
 //---------- BD ---------------------
 define("BD_NOM", "survie_etudiante" );
-define("BD_HOTE", "158.69.192.249" );
+define("BD_HOTE", "localhost" );
 define("BD_PORT", "3307" );
-define("BD_UTILISATEUR", "root");
-define("BD_MDP", "GUEHJCo4");
+define("BD_UTILISATEUR", "projetweb");
+define("BD_MDP", "ProWeb");
 
 require_once($_SERVER["DOCUMENT_ROOT"]."/DAO/ConnexionBD.php");
 
