90914f2fbc0bd1f51a8ea6c6615159719b73c65a
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-02-13 09:22:52 -0500

    objet de index récupéré par requête préparé references #24

--- www/index.php
+++ www/index.php
@@ -3,52 +3,6 @@
 
 	require_once OBJET_DAO;
 	require_once OBJET_MODELE;
-
-
-
-	// Données test
-	$produit1 = new stdClass();
-	$produit1->identifiantDeVente = "ez1ezac15hy8j";
-	$produit1->identifiantVendeur = 4519875102;
-	$produit1->titreDeVente = "Casserole en metal";
-	$produit1->categorie = 0;  // 0 : Ustensiles de cuisine
-	$produit1->prix = 20.0;
-	$produit1->identifiantMonnaie = 0;  // 0 : dollars
-	$produit1->descriptionProduit = "Bonjour, je vends cette casserole qui ne me sert plus, j'habite
-	actuellement au Cégep de Matane";
-	$produit1->detailsVente = "Je ne livre pas et je n'envoie pas par la poste, il faudra venir la chercher à Matane au Cégep";
-	$produit1->adresse = "616 Avenue du Saint-Rédempteur G4W 1L1 Matane QC Québec";
-	$produit1->image = "illustrations/tests/casserole.jpg";
-	
-	$produit2 = new stdClass();
-	$produit2->identifiantDeVente = "wr54u8ix1v18r9";
-	$produit2->identifiantVendeur = 4524803548;
-	$produit2->titreDeVente = "Ensemble de couverts";
-	$produit2->categorie = 0;  // 0 : ustensiles de cuisine
-	$produit2->prix = 12.0;
-	$produit2->identifiantMonnaie = 0;  // 0 : dollarss
-	$produit2->descriptionProduit = "Bonjour, je vends cet ensemble de couverts, ils sont en parfait état";
-	$produit2->detailsVente = "Je peux vous les apporter si vous habitez à québec, sinon il va falloira aller les chercher vous-même";
-	$produit2->image = "illustrations/tests/couverts.jpg";
-	
-	$produit3 = new stdClass();
-	$produit3->identifiantDeVente = "d2j8e62g1j7y9s";
-	$produit3->identifiantVendeur = 45215249;
-	$produit3->titreDeVente = "Vend serviette de bain";
-	$produit3->categorie = 1;  // 1 : Hygiène
-	$produit3->prix = 8.0;
-	$produit3->identifiantMonnaie = 0;  // 0 : dollarss
-	$produit3->descriptionProduit = "Vends serviette, taille 1.5m * 0.7m";
-	$produit3->detailsVente = "Je ne peux pas livrer il va falloir venir les chercher chez moi, j'habite à Montréal";
-	$produit3->image = "illustrations/tests/serviette.jpg";
-	
-	
-	$tableauProduit = array(
-		$produit1,
-		$produit2,
-		$produit3,
-	);
-	
 ?>
 
 
@@ -135,7 +89,7 @@
 				<table id="colonne-centrale-accueil">
 					<tr>
 						<td>
-							<img src="<?=$produitCourant->image?>" class="photo-miniature-produit"/>
+							<img src="<? $objet->getIllustration();?>" class="photo-miniature-produit"/>
 						</td>
 						<td>
 							<table >
