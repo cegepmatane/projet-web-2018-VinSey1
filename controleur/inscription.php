06dfda9af243a76d84ab531cafc75bdb7e45d5ee
Fromage <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-02-16 09:13:28 -0500

    avancement references #28

--- controleur/inscription.php
+++ controleur/inscription.php
@@ -1,9 +1,10 @@
 <?php
+	require_once $_SERVER["DOCUMENT_ROOT"]."/configuration/configuration.dev.php";
 	
 	if (isset($_POST['controleur_inscription'])){
 		
-		$utilisateur = new Utilisateur(0, $_POST["nom"], $_POST["prenom"], $_POST["pseudonyme"], $_POST["email"], $_POST["adresse"], $_POST["codepostal"], $_POST["ville"], 0, 0, $_POST["illustration"], $_POST["anneenaissance"], $_POST["telephone"],);
-		
+		$utilisateur = new Utilisateur(0, $_POST["nom"], $_POST["prenom"], $_POST["pseudonyme"], $_POST["email"], $_POST["adresse"], $_POST["codepostal"], $_POST["ville"], 0, 0, $_POST["illustration"], $_POST["anneenaissance"], $_POST["telephone"]);
+		UTILISATEUR_DAO->insererUtilisateur($utilisateur);
 		
 	}
 
