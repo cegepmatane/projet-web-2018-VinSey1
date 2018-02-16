f08ee1422bd17184c9095569e988b8be62fd387a
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-02-16 09:11:09 -0500

    ajout du signe $ pour les prix

--- catalogue.php
+++ catalogue.php
@@ -55,7 +55,7 @@
 					<img src="<?=$objet->getIllustration();?>" class="photo-miniature-produit"/>
 					<ul>
 						<li><?php echo $objet->getTitreDeVente();?></li>
-						<li><?php echo gettext("Prix");?>: <?php echo $objet->getPrix();?></li>
+						<li><?php echo gettext("Prix");?>: <?php echo $objet->getPrix();?><?php echo gettext(" $");?></li>
 						<li><button type="button"><?php echo gettext("Acheter"); ?></button></li>
 					</ul>
 				</div>
