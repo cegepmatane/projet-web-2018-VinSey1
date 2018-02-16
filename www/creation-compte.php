f69793e5d28b23094661c0f9ea93abe66a953f75
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-02-16 08:40:25 -0500

    References #22

---
+++ www/creation-compte.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width"/>
+    <link rel="stylesheet" href="./decoration/style_general_test.css">
+    <link rel="stylesheet" type="text/css" href="./decoration/MyFontsWebfontsKit.css">		
+	<title> Création de compte</title>
+</head>
+<body>
+    <header>
+        <div id="titre"> Survie étudiante</div>
+        <div id="sous-titre"> Création de compte </div>
+    </header>	
+    <nav>
+        <ul>
+            <li><a href="index.php" title="Aller sur la page d'Accueil">Page d'Accueil</a></li>
+            <li><a href="profil.php" title="Aller sur la page Profil">Page Profil</a></li>
+            <li><a href="catalogue.php" title="Aller sur la page Catalogue">Page Catalogue</a></li>
+            <li><a href="vente.php" title="Aller sur la page Vente">Page Vente</a></li>
+            <li><a href="panneau-administration.php" title="Aller sur la page Panneau d'administration">Page Panneau d'administration</a></li>
+            <li id="page-courante">Page Création de compte</li>
+        </ul>
+    </nav>
+    <form>
+
+    </form>
+</body>
+</html>
