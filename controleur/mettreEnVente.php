3217d0205e185105b9cc3738b268c259a641c9d0
Valentin Dequidt <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-02-17 16:27:44 -0500

    création du script references #31

---
+++ controleur/mettreEnVente.php
@@ -0,0 +1,13 @@
+<?php
+	require_once $_SERVER["DOCUMENT_ROOT"]."/configuration/configuration.dev.php";
+	require_once OBJET_DAO;
+	require_once OBJET_MODELE;
+	
+	if ( isset($_POST['controleur_vente'])){
+		
+		
+		
+	}
+
+
+?>
