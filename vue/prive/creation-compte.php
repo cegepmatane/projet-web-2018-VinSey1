986202bef89411ece0325f421a35f860cff63890
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-02-20 09:25:39 -0500

    Changement Chemin CSS References #35

--- vue/prive/creation-compte.php
+++ vue/prive/creation-compte.php
@@ -6,8 +6,8 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width"/>
-    <link rel="stylesheet" href="./decoration/style_general_test.css">
-    <link rel="stylesheet" type="text/css" href="./decoration/MyFontsWebfontsKit.css">		
+    <link rel="stylesheet" href="../../decoration/style_general_test.css">
+    <link rel="stylesheet" type="text/css" href="../../decoration/MyFontsWebfontsKit.css">		
 	<title> <?php echo gettext("Création de compte") ?></title>
 </head>
 <body>
