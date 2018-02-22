229a4b3c399dd06161e4cf3e78db29e110cdb5b0
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-02-22 18:43:28 -0500

    Ajout du contrôleur permettant de modifier un objet References #43

--- controleur/modifierObjet.php
+++ controleur/modifierObjet.php
@@ -7,6 +7,20 @@
 	if (isset($_POST['controleur_modification_objet'])){
 		$objet = new ObjetDAO();
 		$objet->modifierTitreDeVente($_POST['idObjet'], $_POST['titreDeVente']);
+		$objet->modifierDescriptionProduit($_POST['idObjet'], $_POST['descriptionProduit']);
+		$objet->modifierTitreDeVente($_POST['idObjet'], $_POST['details']);
+		$objet->modifierTitreDeVente($_POST['idObjet'], $_POST['adresse']);
+		$objet->modifierTitreDeVente($_POST['idObjet'], $_POST['prix']);
+		if (isset($_POST['autre'])){
+			$categorie = "autre";
+		}
+		if (isset($_POST['couverts'])){
+			$categorie = "couverts";
+		}
+		if (isset($_POST['literie'])){
+			$categorie = "literie";
+		}
+		$objet->modifierTitreDeVente($_POST['idObjet'], $categorie);
 		
 	}
 
