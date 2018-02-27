b5fc7f70c09be173057de2f7292b638baf21786a
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-02-27 08:57:45 -0500

    configuration staging dans l'en tête references #53

--- vue/prive/entete.php
+++ vue/prive/entete.php
@@ -1,5 +1,5 @@
 <?php
-	require_once $_SERVER["DOCUMENT_ROOT"]."/configuration/configuration.dev.php";
+	require_once $_SERVER["DOCUMENT_ROOT"]."/configuration/configuration.staging.php";
 ?>
 <!DOCTYPE html>
 <html lang="fr">
