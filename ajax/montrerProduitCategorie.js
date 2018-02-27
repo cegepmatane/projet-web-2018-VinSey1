fe51d5ecabbd76e944c4dd5d5e3fdc6e89eb665c
Valentin Dequidt <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-02-27 10:40:41 -0500

    fichier renommé references #54

--- ajax/montrerProduitCategorie.js
+++ ajax/montrerProduitCategorie.js
@@ -4,12 +4,12 @@ function montrerProduitCategorie(categorie){
 	xmlhttp.onreadystatechage = function(){
 		
 		if ( this.readyState == 4 && this.status == 200){
-			
+			document.getElementById('produit-courant').innerHTML = this.responseText;
 		}
 		
 	};
 	
-	xmlhttp.open("GET", "montrerProduitCategorie.php?q="+categorie, true);
+	xmlhttp.open("GET", "listerObjet.php?q="+categorie, true);
 	xmlhttp.send();
 
 }
