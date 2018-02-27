4d188a3709b27d49ce50d212f18ab77c833cca33
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-02-27 08:53:34 -0500

    fichier configuration staging references #53

--- configuration/configuration.staging.php
+++ configuration/configuration.staging.php
@@ -1,9 +1,15 @@
 <?php
 define( "UTILISATEUR_DAO", $_SERVER["DOCUMENT_ROOT"]."/DAO/UtilisateurDAO.php"   );
 define( "UTILISATEUR_MODELE", $_SERVER["DOCUMENT_ROOT"]."/modele/Utilisateur.php"   );
+define( "UTILISATEUR_CONTROLEUR", $_SERVER["DOCUMENT_ROOT"]."/controleur/controleurUtilisateur.php" );
 
 define( "OBJET_DAO", $_SERVER["DOCUMENT_ROOT"]."/DAO/ObjetDAO.php"   );
 define( "OBJET_MODELE", $_SERVER["DOCUMENT_ROOT"]."/modele/Objet.php"   );
+define( "OBJET_CONTROLEUR", $_SERVER["DOCUMENT_ROOT"]."/controleur/controleurObjet.php" );
+
+define( "LISTE_ERREUR_FORMULAIRE", $_SERVER["DOCUMENT_ROOT"]."/listeErreurFormulaire.php" );
+
+define("INSCRIPTION_UTILISATEUR_CONTROLEUR",$_SERVER["DOCUMENT_ROOT"]."/controleur/controleurInscription.php" );
 
 //---------- BD ---------------------
 define("BD_NOM", "survie_etudiante" );
@@ -28,7 +34,6 @@ setlocale(LC_ALL, $language);
 $domain = "messages";
 bindtextdomain($domain, "Locale");
 textdomain($domain);
-
 
 
 
