59ede7a8e9763f09aa8b27774d6e65d8b7ea2642
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-03-06 14:39:25 -0500

    changement  pour w3c references #63

--- listeObjets.php
+++ listeObjets.php
@@ -1,6 +1,6 @@
 <?php function listeObjets ($objet){ ?>
     <div class="produit-courant">
-        <img src="<?=$objet->getIllustration();?>" class="photo-miniature-produit"/>
+        <img src="<?=$objet->getIllustration();?>" class="photo-miniature-produit" alt="image survie étudiante"/>
         <ul>
             <li><?php echo $objet->getTitreDeVente();?></li>
             <li><?php echo gettext("Prix");?>: <?php echo $objet->getPrix();?><?php echo gettext(" $");?></li>
