d417fe0f93fecde60eb364b76a7f1a582ac5ae6d
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-03-07 17:27:54 -0500

    Ajout simulation vente Stripe pour objet catégorie et index References #56

--- listeObjets.php
+++ listeObjets.php
@@ -1,15 +1,20 @@
-<?php function listeObjets ($objet){ ?>
+<?php 
+include "/transactions/test_stripe.php";
+function listeObjets ($objet){ ?>
     <div class="produit-courant">
         <img src="<?=$objet->getIllustration();?>" class="photo-miniature-produit" alt="image survie étudiante"/>
         <ul>
             <li><?php echo $objet->getTitreDeVente();?></li>
             <li><?php echo gettext("Prix");?>: <?php echo $objet->getPrix();?><?php echo gettext(" $");?></li>
-            <li>
-                <form action="achat.php" method="post">
-                    <input type="hidden" name="idObjet" value="<?php echo $objet->getIdObjet(); ?>"/>
-                    <input type="submit" value="Acheter"/>
-                </form>
-            </li>
+            <!--
+                <li>
+                    <form action="achat.php" method="post">
+                        <input type="hidden" name="idObjet" value="<?php echo $objet->getIdObjet(); ?>"/>
+                        <input type="submit" value="Acheter"/>
+                    </form>
+                </li>
+            -->
+            <li><?php paiement($objet); ?></li>
         </ul>
     </div>
 <?php } ?>
---
+++ transactions/test_stripe.php
@@ -0,0 +1,14 @@
+<?php function paiement($objet) { ?>
+    <form action="your-server-side-code" method="POST">
+    <script
+        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+        data-key="pk_test_yXhm1rmmGnCVqBauOp3ZTqeJ"
+        data-amount="<?php echo $objet->getPrix()*100; ?>"
+        data-name="<?php echo gettext('Survie étudiante') ?>"
+        data-description="<?php echo $objet->getTitreDeVente();?>"
+        data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+        data-locale="auto"
+        data-currency="cad">
+    </script>
+    </form>
+<?php } ?>
