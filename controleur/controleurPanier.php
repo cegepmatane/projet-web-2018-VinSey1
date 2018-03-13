f291973b86fe940af78a7a23fe8f63992d67d719
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-03-13 14:55:29 -0400

    Correction synthaxe #References 69

--- controleur/controleurPanier.php
+++ controleur/controleurPanier.php
@@ -1,26 +1,26 @@
 <?php session_start();
 class Panier {
     
-    protected $contenu_panier = array();
+    protected $contenuPanier = array();
 
     //Construction du panier en fonction de la session
     public function construction(){
         //Récupère le contenu du panier de la session
-        $this->contenu_panier = !empty($_SESSION['contenu_panier'])?$_SESSION['contenu_panier']:NULL;
-        if($this->contenu_panier == NULL){
+        $this->contenuPanier = !empty($_SESSION['contenuPanier'])?$_SESSION['contenuPanier']:NULL;
+        if($this->contenuPanier == NULL){
             //Si il n'a pas de panier, définit les valeurs pas défaut
-            $this->contenu_panier = array('prix_total' => 0, 'items_total' => 0);
+            $this->contenuPanier = array('prixTotal' => 0, 'itemsTotal' => 0);
         }
     }
 
     //Fonction qui retourne l'ensemble du contenu du panier
     public function contenu(){
         //Effectue un tri avec l'ajout le plus récent en premier
-        $panier = array_reverse($this->contenu_panier);
+        $panier = array_reverse($this->contenuPanier);
 
         //Enlève le nombre total d'items et le prix total pour ne pas créer d'erreurs
-        unset($panier['prix_total']);
-        unset($panier['items_total']);
+        unset($panier['prixTotal']);
+        unset($panier['itemsTotal']);
 
         return $panier;
     }
