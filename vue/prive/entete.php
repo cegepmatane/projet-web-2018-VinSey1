38a3cf30d37a8f21bd4d6dd4940964f33dcbcc43
Valentin Dequidt <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-03-13 17:33:59 -0400

    avancement references #67

--- vue/prive/entete.php
+++ vue/prive/entete.php
@@ -27,10 +27,10 @@
     </header>	
     <nav>
         <ul>
-            <li><a href="../../index.php" title="<?php echo gettext("Aller sur la page d'Accueil")?>"><?php echo gettext("Page d'Accueil")?></a></li>
+            <li><a href="../../index.php" title="<?php echo gettext("Aller sur la page d'Accueil")?>"><?php echo gettext("Accueil")?></a></li>
             <li><a href="../../catalogue.php" title="<?php echo gettext("Aller sur la page Catalogue")?>"><?php echo gettext("Page Catalogue")?></a></li>
-			<li><a href="administration-utilisateur.php" title="<?php echo gettext("Aller sur la page Administration utilisateur")?>"><?php echo gettext("Page Administration utilisateur")?></a></li>
-			<li><a href="administration-objet.php" title="<?php echo gettext("Aller sur la page Administration Objet")?>"><?php echo gettext("Page Administration objet")?></a></li>
+			<li><a href="administration-utilisateur.php" title="<?php echo gettext("Aller sur la page Administration utilisateur")?>"><?php echo gettext("dministration utilisateur")?></a></li>
+			<li><a href="administration-objet.php" title="<?php echo gettext("Aller sur la page Administration Objet")?>"><?php echo gettext("Administration objet")?></a></li>
 			<li><a href="panneau-administration.php" title="<?php echo gettext("Aller sur le panneau d'administration")?>"><?php echo gettext("Panneau administration")?></a></li>
 		</ul>
     </nav>
