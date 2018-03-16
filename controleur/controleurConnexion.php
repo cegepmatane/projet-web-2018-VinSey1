8869ff694561af895c064ba978fd8328b1bb0b02
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-03-15 23:25:41 -0400

    controleurConnexion qui vérifier la corrélation entre mdp et pseudo references #72

--- controleur/controleurConnexion.php
+++ controleur/controleurConnexion.php
@@ -4,4 +4,44 @@
 	require_once UTILISATEUR_MODELE;
 	
 	
+	if ( isset($_POST['actionFormulaire'])){
+		
+		$utilisateurDAO = new UtilisateurDAO();
+		$utilisateurDAO->recupererInformationConnexion($_POST['pseudonyme']);
+		$isPasswordCorrect = password_verify($_POST['motdepasse'], $resultat['motdepasse']);
+		
+		if (!$resultat)
+
+		{
+
+			echo 'Mauvais identifiant ou mot de passe !';
+
+		}
+
+		else
+
+	{
+
+		if ($isPasswordCorrect) {
+
+			session_start();
+
+			$_SESSION['id'] = $resultat['id_utilisateur'];
+
+			$_SESSION['pseudo'] = $pseudonyme;
+
+			echo 'Vous êtes connecté !';
+
+		}
+
+		else {
+
+			echo 'Mauvais identifiant ou mot de passe !';
+
+		}
+
+		}
+	}
 	
+?>
+		
