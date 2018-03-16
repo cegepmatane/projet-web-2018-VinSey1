7ec8def83291b569b35dda9160537a1cb499cfa4
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-03-15 22:53:43 -0400

    formulaire de connexion crée references #72

--- connexion.php
+++ connexion.php
@@ -7,6 +7,22 @@
 	
 	
 ?>
+
+
+<form action="connexion.php" id="formulaire-creation" method="post">
+			<div id = "blocs-formulaire">
+				<div id = "bloc-formulaire-1">
+					<ul>
+						<li>
+							<label for="pseudonyme"> <?php echo gettext("Pseudonyme"); ?>: </label>
+							<input type="text" name="pseudonyme" id="pseudonyme" value="<?php if ( $utilisateur) echo $utilisateur->getPseudonyme(); ?>" />
+																		
+						</li>
+						<li>
+							<label for="motdepasse"> <?php echo gettext("Mot de passe"); ?>: </label>
+							<input type="password" name="motdepasse" id="motdepasse" value="<?php if ( $utilisateur) echo $utilisateur->getMotDePasse(); ?>" />  
+																			
+						</li>
 <?php 
 	include "piedPage.php";
 ?>
