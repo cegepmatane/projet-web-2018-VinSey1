f2009590bc32a2fff8ac3dc399464db9de3668b2
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-03-16 21:41:20 -0400

    création du controleur de la barre de recherche references #75

--- barreDeRecherche.php
+++ barreDeRecherche.php
@@ -1,8 +1,25 @@
+<?php
+
+	include "entete.php";
+	
+	require_once RECHERCHE_UTILISATEUR_CONTROLEUR;
+	
+	
+?>
+
+
+
+
 <div id="barre-de-recherche" >
 		<img src=./illustrations/petit/loupe.png id="image-loupe" alt="barre de recherche survie étudiante">
 		<form action="barreDeRecherche.php" method="Post">
 			<input type="text" name="nato_pf"/>
-			<input type="submit" name="chercher" value="<?php echo gettext("Chercher")?>" />
+			<input type="submit" name="chercher" value="<?php echo gettext("Chercher")?>" name="actionFormulaire" />
 		</form>
 		<img src=./illustrations/petit/loupe.png id="image-loupe" alt="barre de recherche survie étudiante">	
 </div>
+
+
+<?php 
+	include "piedPage.php";
+?>
