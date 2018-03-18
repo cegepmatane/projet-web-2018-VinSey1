8582d92e9d3fd386b8b05f4a80c8e6902d43db8d
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-03-18 19:05:28 -0400

    suppression d'un graphe references #55

--- vue/prive/graph.php
+++ vue/prive/graph.php
@@ -128,34 +128,5 @@
 				};
 				myChart.setOption(option);
 			</script>
-			<div id="chart3" style="width: 500px; height: 350px;"></div>
-			<script type="text/javascript">
-				var myChart = echarts.init(document.getElementById('chart3'));
-				var option = {
-					
-						tooltip : {
-							formatter: "{a} <br/>{b} : {c}%"
-						},
-						toolbox: {
-							feature: {
-								restore: {},
-								saveAsImage: {}
-							}
-						},
-						series: [
-							{
-								name: 'Performance',
-								type: 'gauge',
-								detail: {formatter:'{value}%'},
-								data: [{value: 50, name: 'Performance'}]
-							}
-						]
-					};
-
-					setInterval(function () {
-							option.series[0].data[0].value = (Math.random() * 100).toFixed(2) - 0;
-							myChart.setOption(option, true);
-							},2000);
-				myChart.setOption(option);
-			</script>
+			
 		</div>
