4536e148fd7d1d3d6eba2471127ad2b235d7cc7b
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-03-18 22:17:23 -0400

    Ajout incrémentation nombre d'achats après achat objet References #56

--- retour-achat.php
+++ retour-achat.php
@@ -2,21 +2,40 @@
 
 	include "entete.php";
 	require_once OBJET_DAO;
+	require_once UTILISATEUR_DAO;
+	require_once UTILISATEUR_MODELE;
 	require_once OBJET_MODELE;
 
 	$objetDAO = new ObjetDAO();
 
-	$idObjet = 1;
+	$utilisateurDAO = new UtilisateurDAO();
 
 	if ( isset($_GET['idObjet']) ){
 		$idObjet = $_GET['idObjet'];
+	} else {
+		?> <script type='text/javascript'>document.location.replace('index.php');</script> <?php
 	}
 
 	$objet = $objetDAO->chercherParIdentifiant($idObjet);
-?>
-<html>
-<body>
-	<h3>Merci d'avoir acheté l'objet suivant : <?php echo $objet->getTitreDeVente();?></h3>
-</body>
-</html>
-<?php include "piedPage.php"; ?>
+
+	if (isset($_SESSION['id']) && isset($_SESSION['pseudonyme'])) { 
+
+		$utilisateur = $utilisateurDAO->chercherParIdentifiant($_SESSION['id']);
+
+		$utilisateur->setNbachats($utilisateur->getNbachats() +1);
+
+		if($utilisateur->estValide()){
+			$utilisateurDAO->modifierUtilisateur($utilisateur);
+		}
+
+		?>
+	<html>
+	<body>
+		<h3><?php echo gettext("Merci d'avoir acheté l'objet suivant : ").$objet->getTitreDeVente();?></h3>
+		<a href="index.php"><?php echo gettext("Retour à l'accueil");?></a>
+	</body>
+	</html>
+	<?php } else {
+		?> <script type='text/javascript'>document.location.replace('index.php');</script> <?php
+	}
+	include "piedPage.php"; ?>
