44fbe755203c7c9ec5fb89e06c711754587b3927
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-03-18 22:06:08 -0400

    création du controleur pour mettre l'objet en vente references #79

---
+++ controleur/controleurVente.php
@@ -0,0 +1,18 @@
+<?php
+
+	require_once OBJET_DAO;
+	require_once OBJET_MODELE;
+
+	
+	if ( isset($_POST['actionFormulaire'])){
+		
+		$objet = new Objet();
+		
+		$objet->setCategorie($_POST['categorie']);
+		$
+		
+		
+	}
+	
+	
+?>
