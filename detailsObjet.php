71c2bafc488ee7552c96a06ae5b63a5a02e0d757
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-03-21 11:41:08 -0400

    modification pour qu'on ne puisse pas acheter un objet déjà vendu references #55

--- detailsObjet.php
+++ detailsObjet.php
@@ -6,7 +6,10 @@ function detailsObjet ($objet){ ?>
         <ul>
             <li><?php echo $objet->getTitreDeVente();?></li>
             <li><?php echo gettext("Prix");?>: <?php echo $objet->getPrix();?><?php echo gettext(" $");?></li>
+			<?php if (!$objet->getVedette()==10) {
+			?>
             <li><a class="lienbouton" href="acheter.php?idObjet=<?php echo $objet->getIdObjet();?>" >Acheter Maintenant !</a></li>
+			<?php } ?>
         </ul>
     </div>
 <?php } ?>
