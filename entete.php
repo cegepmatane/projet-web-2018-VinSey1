dc1dd33160f402b71372e594871488b5f4d832f4
Valentin Dequidt <31954676+PaillassonAsthmatique@users.noreply.github.com>
2018-03-21 10:19:58 -0400

    references #82 ajout redirection "accueil"

--- entete.php
+++ entete.php
@@ -49,7 +49,7 @@
     </header>
 	<nav>
 		<ul>
-			<li><a href="index.php" title="<?php echo gettext("Aller sur la page d'Accueil")?>"><?php echo gettext("Accueil")?></a></li>
+			<li><a href="accueil" title="<?php echo gettext("Aller sur la page d'Accueil")?>"><?php echo gettext("Accueil")?></a></li>
 			<li><a href="catalogue.php" title="<?php echo gettext("Aller sur la page Catalogue")?>"><?php echo gettext("Catalogue")?></a></li>
 			<?php if (isset($_SESSION['id']) && isset($_SESSION['pseudonyme'])){ ?>
 					<li><a href="vue/prive/profil.php" title="<?php echo gettext("Aller sur la page Profil")?>"><?php echo gettext("Profil")?></a></li>
