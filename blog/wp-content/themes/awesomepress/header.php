b2e831839a5d1f2c6759c12ea82d68e075a647ce
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-03-21 16:36:09 -0400

    tests

--- blog/wp-content/themes/awesomepress/header.php
+++ blog/wp-content/themes/awesomepress/header.php
@@ -1,6 +1,4 @@
 <?php
-
-session_start();
 /**
  * The header for our theme.
  *
@@ -20,9 +18,7 @@ session_start();
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<title><?php echo gettext("Survie étudiante") ?></title>
-<link rel="stylesheet" href="../../decoration/style_general.css">
-<link rel="stylesheet" type="text/css" href="../../decoration/MyFontsWebfontsKit.css">	
+
 <?php awesomepress_head_bottom(); ?>
 <?php wp_head(); ?>
 </head>
@@ -35,14 +31,7 @@ session_start();
     <?php awesomepress_header_before(); ?>
     <header id="masthead" class="site-header" role="banner">
     <?php awesomepress_header_top(); ?>
-    <div id="titre"> <?php echo gettext("Survie étudiante") ?></div>
-        <div id="sous-titre"><?php echo gettext("Partie Publique") ?> </div>
-        <div id="date"><?php  setlocale (LC_TIME, 'fr_FR.utf8','fra'); echo (strftime("%A %d %B"));  ?>
-        <?php 
-		if (isset($_SESSION['id']) && isset($_SESSION['pseudonyme'])) {
-		?>
-		<div id="session"> <?php echo gettext($_SESSION['pseudonyme']) ?>
-	<?php } ?>
+
         <div class="site-branding">
             <?php the_custom_logo(); ?>
             <?php if (is_front_page() && is_home() ) : ?>
@@ -58,19 +47,6 @@ session_start();
         </div><!-- .site-branding -->
 
         <nav id="site-navigation" class="main-navigation" role="navigation">
-                <ul>
-                    <li><a href="accueil" title="<?php echo gettext("Aller sur la page d'Accueil")?>"><?php echo gettext("Accueil")?></a></li>
-                    <li><a href="catalogue" title="<?php echo gettext("Aller sur la page Catalogue")?>"><?php echo gettext("Catalogue")?></a></li>
-                    <li><a href="blog" title="<?php echo gettext("Aller sur le blog")?>"><?php echo gettext("Blog")?></a></li>
-                    <li><a href="doc/page-mission.html" title="<?php echo gettext("Aller sur la page mission")?>"><?php echo gettext("Page mission")?></a></li>
-                    <?php if (isset($_SESSION['id']) && isset($_SESSION['pseudonyme'])){ ?>
-                            <li><a href="vue/prive/profil.php" title="<?php echo gettext("Aller sur la page Profil")?>"><?php echo gettext("Profil")?></a></li>
-                        <?php if ($_SESSION['role'] == 1){  ?>
-                            <li><a href="vue/prive/administration-utilisateur.php" title="<?php echo gettext("Aller sur la page Administration utilisateur")?>"><?php echo gettext("Administration utilisateur")?></a></li>
-                            <li><a href="vue/prive/administration-objet.php" title="<?php echo gettext("Aller sur la page Administration Objet")?>"><?php echo gettext("Administration objet")?></a></li>
-                            <li><a href="vue/prive/panneau-administration.php" title="<?php echo gettext("Aller sur le panneau d'administration")?>"><?php echo gettext("Panneau d'administration")?></a></li>
-                    <?php }} ?>
-                </ul> 
             <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
                 <?php if (AWESOMEPRESS_SUPPORT_FONTAWESOME ) : ?>
                     <i class="fa fa-reorder" aria-hidden="true"></i>
